e8fd93c4270515d1aba40bb5d589d6128a91b038
Shivam-codes247 <shivam.code247@gmail.com>
2026-09-01 21:10:37 +0530

    Initial project upload

---
+++ basics/class01.py
@@ -0,0 +1,38 @@
+# ==========================================
+# CLASS 01 - PYTHON BASICS
+# ==========================================
+
+# Comments
+# Comments are ignored by Python.
+# They are used to explain the code.
+
+#variable is a named memory location 
+
+name = "shivam"
+age = 23
+
+#formatted string 
+print(f"My name is {name} and i am {age} years old")
+
+#naming convention
+#python is case sensetive so all 3 are different variable names
+student_name = "rohit" #snake case
+studentName = "shivam" #camel case
+StudentName = "binit" #pascal case
+
+
+#data type
+
+a = 4           #int
+b = "shivam"    #str
+c = 12.5        #float
+d = True        #bool
+
+print(a,b,c,d)
+
+#checking datatype
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
